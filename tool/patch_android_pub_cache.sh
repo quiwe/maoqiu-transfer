423e83926bf9eb73da7b084ac7991ac250664762
袋小凡 <quiwe@qq.com>
2026-05-19 17:19:15 +0800

    Patch Groovy compile SDK syntax

--- tool/patch_android_pub_cache.sh
+++ tool/patch_android_pub_cache.sh
@@ -9,7 +9,7 @@ if [[ ! -d "$pub_cache" ]]; then
 fi
 
 while IFS= read -r -d '' gradle_file; do
-  perl -0pi -e 's/compileSdk\s*=\s*flutter\.compileSdkVersion/compileSdk = 36/g; s/compileSdkVersion\s+flutter\.compileSdkVersion/compileSdkVersion 36/g; s/compileSdk\s*=\s*\d+/compileSdk = 36/g; s/compileSdkVersion\s+\d+/compileSdkVersion 36/g' "$gradle_file"
+  perl -0pi -e 's/compileSdk\s*=\s*flutter\.compileSdkVersion/compileSdk = 36/g; s/compileSdkVersion\s+flutter\.compileSdkVersion/compileSdkVersion 36/g; s/compileSdk\s*=\s*\d+/compileSdk = 36/g; s/compileSdk\s+\d+/compileSdk 36/g; s/compileSdkVersion\s+\d+/compileSdkVersion 36/g' "$gradle_file"
 done < <(find "$pub_cache" -path "*/android/build.gradle*" -print0)
 
 echo "Android plugin Gradle files patched to compileSdk 36."
